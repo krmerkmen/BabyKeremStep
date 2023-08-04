1ebb109df785188ded38f1ae445760eaa71a8de0
krmerkmen <141085402+krmerkmen@users.noreply.github.com>
2023-08-05 00:37:31 +0300

    Add files via upload

--- 01_Lab.py
+++ 01_Lab.py
@@ -1,21 +1,35 @@
 # region Tax-Free Hesaplama
 
-price = float(input('Toplam Tutar ---> '))
-cash_back = price*0.18
-if price < 118:
-    print('Tax-Free İçin Total Ücret 118TL den Yüksek Olmalıdır')
-else:
-    a = input('Tax-Free İster Misiniz ? ').lower()
-    if a == 'evet':
-        print(f"Cashback miktarı {cash_back}TL'dir")
+# price = float(input('Toplam Tutar ---> '))
+# cash_back = price*0.18
+# if price < 118:
+#     print('Tax-Free İçin Total Ücret 118TL den Yüksek Olmalıdır')
+# else:
+#     a = input('Tax-Free İster Misiniz ? ').lower()
+#     if a == 'evet':
+#         print(f"Cashback miktarı {cash_back}TL'dir")
 
 # endregion
 
 
-# region
+# region İnternet Cafe
 
+# Bir internet kafede 3 farklı class olsun
+# 1. Silver saatlik ücret 20Tl
+# 2. Gold 28Tl
+# 3. Platinium 35Tl
+# Kullanıcıdan öncelikle class seçimi ardından kullanılacak süre alınsın.
+# Süre tam saatlerden oluşmalıdır. 60 dakikanın üstünde kalan dakikalar 60 dakika olarak değerlendirilir. örneğin 51dk = 1 saat
 
-
-
+# c = input ('Bir Class Seçiniz\n1.Silver\n2.Gold\n3.Platinium\n--->')
+# t = int(input('Oturum Sürenizi Seçiniz'))
+#
+# if c == '1' :
+#     print(f'Ücret : {20*t} ')
+# elif c == '2' :
+#     print(f'Ücret : {28 * t} ')
+# elif c == '3' :
+#     print(f'Ücret : {20*t} ')
 
 # endregion
+
